9bb449a6d4050d2d73ec601bb0b0e507a8b2f065
JoesphAguilar <josephjd93@gmail.com>
2022-03-30 13:07:48 -0700

    Have Forms rendering

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import UserForm from './UserForm';
 
 import logo from './logo.svg';
 import './App.css';
 
+// Initial States
+const InitialFormValues = {
+  first_name: '',
+  last_name: '',
+  role: '',
+  email: '',
+  password: '',
+  tos: false,
+};
+const initalFormErrors = {
+  first_name: '',
+  last_name: '',
+  role: '',
+  email: '',
+  password: '',
+};
+const initalUser = [];
+const initalDisabled = true;
+
 function App() {
+  const [user, setUser] = useState(initalUser);
+  const [formValues, setFormValues] = useState(InitialFormValues);
+  const [formErrors, setFormErrors] = useState(initalFormErrors);
+  const [disabled, setDisabled] = useState(initalDisabled);
+  
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User Onboarding App</h1>
+
+      <UserForm 
+        values={formValues}
+      />
+
     </div>
   );
 }
--- user-onboarding/src/UserForm.js
+++ user-onboarding/src/UserForm.js
@@ -1,5 +1,105 @@
 import React from "react";
 
 export default function UserForm(props) {
-    return; 
+    const {
+        values,
+        submit,
+        change,
+        disabled,
+        errors,
+    } = props
+
+    const onSubmit = evt => {
+        evt.preventDefault();
+        submit();
+    }
+
+    const onChange = evt => {
+        
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <h2>Add a User</h2>
+
+            {/*Disable Button here */}
+
+            {/*Render Validation errors here */}
+
+            <div>
+                <h4>General Information</h4>
+                <label>First Name
+                    <input 
+                        value={values.first_name}
+                        onChange={onChange}
+                        name='firstName'
+                        type='text'
+                    />
+                </label>
+
+                <label>Last Name
+                    <input
+                        value={values.last_name}
+                        onChange={onChange}
+                        name='lastName'
+                        type='text'
+                    />
+                </label>
+
+                <label>Role 
+                    <select 
+                        onChange={onChange}
+                        value={values.role}
+                        name='role'
+                    >
+                         <option value="">-- Select A Role --</option>
+                        <option value="Web Developer">Web Developer</option>
+                        <option value="Software Engineer">Software Engineer</option>
+                        <option value="Software Developer">Software Developer</option>
+                        <option value="Front End Developer">Front End Developer</option>
+                        <option value="Network Engineer">Network Engineer</option>
+                        <option value="Full Stack Developer">Full Stack Developer</option>
+                    </select>
+                </label>
+
+                <label>Email
+                    <input
+                       value={values.email}
+                       onChange={onChange} 
+                       name='email'
+                       type='text'
+                    />
+                </label>
+
+                <label>Password
+                    <input
+                    value={values.password}
+                    onChange={onChange}
+                    name='password'
+                    type='password'
+                    />
+                </label>
+            </div>
+            <div>
+                <h4>Terms of Service</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                     ut labore et dolore magna aliqua. At varius vel pharetra vel turpis nunc eget. Eget nunc
+                      scelerisque viverra mauris in aliquam sem fringilla ut. Volutpat consequat mauris nunc 
+                      congue nisi vitae suscipit. Phasellus faucibus scelerisque eleifend donec pretium vulputate 
+                      sapien. Est velit egestas dui id ornare. Libero volutpat sed cras ornare arcu dui vivamus. 
+                      Sed odio morbi quis commodo. Vivamus arcu felis bibendum ut tristique et egestas quis ipsum. 
+                      Condimentum mattis pellentesque id nibh tortor id. Placerat in egestas erat imperdiet sed. 
+                      Ut venenatis tellus in metus.</p>
+                
+                <label>I Agree to Terms
+                    <input
+                        type='checkbox'
+                        name='tos'
+                        onChange={onChange}
+                        checked={values.tos}
+                    />
+                </label>
+            </div>
+        </form>
+    )
 }
